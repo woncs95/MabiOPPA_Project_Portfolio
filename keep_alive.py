1aace7a7feeab4627f596d9856a5c1e83c233ef9
csw <woncs95@gmail.com>
2021-04-14 22:08:39 +0200

    first push pycharm

---
+++ keep_alive.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Hello. I am alive!"
+
+def run():
+  app.run(host='ftp.gear.host',port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
